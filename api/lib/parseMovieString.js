01ce1d50253bc14b1525b1a888ab66d7afa923b6
Serhii Larchenko <serhii.larchenko@gmail.com>
2018-12-05 11:01:05 +0200

    Fixed: Windows file bug

--- api/lib/parseMovieString.js
+++ api/lib/parseMovieString.js
@@ -1,7 +1,9 @@
 function parseMovieString (data) {
-  return data.toString().trim().split('\n\n').map(string => {
+
+  return data.toString().trim().split('\nTitle:').map((string,i) => {
+    if (i !== 0) string = 'Title:' + string;
     let movie = {};
-    string.split('\n').forEach(feature => {
+    string.split('\n').forEach( (feature, i) => {
       let limit = feature.indexOf(':');
       movie[feature.slice(0,limit).toLowerCase()] = 
       feature.slice(limit+1).trim();
